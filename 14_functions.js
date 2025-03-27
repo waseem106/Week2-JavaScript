f9de0db161b52ddacf5fed64addff2dc05dba372
Waseem Aziz <Waseemaziz106@gmail.com>
2025-03-27 11:23:13 +0500

    updated functions file

--- 14_functions.js
+++ 14_functions.js
@@ -4,3 +4,31 @@ function sum(a,b)
 }
 
 console.log(sum(2, 4))
+
+
+
+
+//rest operator will wrap up the things and give us array
+
+function calculateCartPrice(...price){
+    return price.reduce((total,value)=>{
+        return total=total+value
+    })
+    
+}
+console.log(calculateCartPrice(2,4,4))
+
+
+
+
+function testingScope()
+{
+    var num=12
+    let num2= 20
+    const num3=30
+
+}
+
+// console.log(num)
+// console.log(num2)
+// console.log(num3)
